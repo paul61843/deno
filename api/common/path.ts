beae4bcad36d4de9b2dc7e338187bb550a99b487
paul61843 <paul61843@gmail.com>
2022-07-01 15:26:41 +0000

    feat: 新增搜尋縣市天氣API

--- api/common/path.ts
+++ api/common/path.ts
@@ -2,3 +2,4 @@ export const WEATHER_FORECAST_THREE = '/v1/rest/datastore/F-C0032-001';
 
 // server api path
 export const WEATHER_TODAY = '/weather/today';
+export const WEATHER_TODAY_Search = '/weather/today/search';
--- main.tsx
+++ main.tsx
@@ -4,7 +4,7 @@ import { WeatherAPI, ServerAPI } from "./api/api.ts";
 import { appHtml } from "./pages/index.tsx";
 import { React, ReactDOMServer } from "./dep.ts";
 import { App, Index } from "./pages/index.tsx";
-import { WEATHER_TODAY } from "./api/common/path.ts";
+import { WEATHER_TODAY, WEATHER_TODAY_Search } from "./api/common/path.ts";
 
 const app = new Application();
 const router = new Router();
@@ -23,6 +23,31 @@ router.get(WEATHER_TODAY, async ({ response }) => {
   try {
     const weatherAPI = new WeatherAPI();
     response.body = await weatherAPI.getTodayWeather();
+    response.status = 200;
+  } catch (error) {
+    response.status = 404;
+    response.body = {
+      success: false,
+      message: "No Found",
+    };
+    console.warn(error);
+  }
+});
+
+router.get(WEATHER_TODAY_Search, async ({ request, response }) => {
+  try {
+    const queryString = new URLSearchParams(decodeURI(request?.url?.search));
+    const cities = queryString.get("cities");
+    const requestContent = cities ? cities.split(",") : [];
+    const weatherAPI = new WeatherAPI();
+    const todayWeather = await weatherAPI.getTodayWeather();
+    const location = todayWeather?.records?.location;
+    const searchResult = location.filter(
+      ({ locationName }: { locationName: string }) =>
+        requestContent.includes(locationName)
+    );
+
+    response.body = searchResult;
     response.status = 200;
   } catch (error) {
     response.status = 404;
