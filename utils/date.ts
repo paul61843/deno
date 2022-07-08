d70c16e770fe4abddf28e51fb8e3e7a2fb317b92
paul61843 <paul61843@gmail.com>
2022-07-08 00:59:54 +0000

    chore: 移除不必要的註解

--- utils/date.ts
+++ utils/date.ts
@@ -1,7 +1,7 @@
 export function getToday(date = '', format = '') {
     const newDate = date ? new Date(date) : new Date();
     const dd = String(newDate.getDate()).padStart(2, '0');
-    const mm = String(newDate.getMonth() + 1).padStart(2, '0'); //January is 0!
+    const mm = String(newDate.getMonth() + 1).padStart(2, '0');
     const yyyy = newDate.getFullYear();
 
     return `${yyyy}${mm}${dd}`;
